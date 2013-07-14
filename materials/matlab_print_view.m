7a18120e9063f4629f69622909e9ab9eef7e4561
Aleksander Budzinski <aleksander.budzinski@mechatronics.it.cx>
2013-07-14 11:59:24 +0200

    print modified

--- materials/matlab_print_view.m
+++ materials/matlab_print_view.m
@@ -1,5 +1,5 @@
 surf(b04103f40x2D68900x2D421c0x2D86a30x2D2f787c4ba5ae_result,'DisplayName','b04103f40x2D68900x2D421c0x2D86a30x2D2f787c4ba5ae_result');figure(gcf)
-
+%%
 set(gca,'XTickLabel',{'-150','-100','-50','0','50','100','150'})
 set(gca,'XTick',0:50:300)
 set(gca,'YTickLabel',{'-150','-100','-50','0','50','100','150'})
@@ -11,3 +11,20 @@ zLabel('presence probability')
 title('Example simulation result for 3 active nodes')
 
 view([-35,-60,170]);
+
+axis square
+view([0, 0, 90]);
+xlim([50,300])
+ylim([1,250])
+
+%%
+
+xLabel('position in x axis - no unit')
+yLabel('position in y axis - no unit')
+zLabel('presence probability - no unit')
+title('Single sub matrix with probability curve - zoom')
+
+axis square
+view([0, 0, 90]);
+xlim([75,150])
+ylim([175,220])
